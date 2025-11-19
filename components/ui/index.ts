4dbc3897c34fca45a4a0e7debc315fb4b7d4e299
余磊 <a2691218460@outlook.com>
2025-11-19 12:09:39 +0800

    修复了构建错误

---
+++ components/ui/index.ts
@@ -0,0 +1,22 @@
+// 基础UI组件 (使用默认导出)
+export { default as Button } from './Button';
+export { default as GlowCard } from './GlowCard';
+export { default as TerminalCard } from './TerminalCard';
+export { default as CodeShowcase } from './CodeShowcase';
+export { default as ProfileImage } from './ProfileImage';
+export { default as ThemeToggle } from './ThemeToggle';
+export { default as VideoPlayer } from './VideoPlayer';
+export { default as ImageCarousel } from './ImageCarousel';
+
+// 动画组件
+export { TypewriterText } from './TypewriterText';
+
+// Lottie动画组件
+export { LottieJS } from './LottieJS';
+export { LottieBuild } from './LottieBuild';
+export { LottieCode } from './LottieCode';
+export { LottieCoding } from './LottieCoding';
+export { LottieContact } from './LottieContact';
+export { LottieDevelopment } from './LottieDevelopment';
+export { LottieShowcase } from './LottieShowcase';
+export { LottieStudy } from './LottieStudy';
